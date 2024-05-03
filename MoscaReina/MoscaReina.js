3ec10c9abe78d5cfeb34f938bac0d846bb007c2e
jflrubio4 <jflrubio@correo.ugr.es>
2024-05-03 03:13:33 +0200

    Mosca Reina

--- MoscaReina/MoscaReina.js
+++ MoscaReina/MoscaReina.js
@@ -1,7 +1,8 @@
 import * as THREE from '../libs/three.module.js'
 import {CSG} from '../libs/CSG-v2.js'
+import { Mosca } from '../Mosca/Mosca.js';
  
-class Mosca extends THREE.Object3D {
+class MoscaReina extends THREE.Object3D {
   constructor(gui,titleGui) {
     super();
     
@@ -12,8 +13,13 @@ class Mosca extends THREE.Object3D {
     //DEFINIMOS LE MATERIAL.
     var mat = new THREE.MeshNormalMaterial();
 
-    //CUERPO MOSCA
-    var cuerpoGeom = new THREE.SphereGeometry(0.5, 32, 32);
+    // var mosca = new Mosca(gui, "Controles Mosca");
+    // // Escalar la corona con la mosca
+    // mosca.scale.set(1.5, 1.5, 1.5);
+    // this.add(mosca);
+
+    //CODIGO DE LA MOSCA
+    var cuerpoGeom = new THREE.SphereGeometry(1, 32, 32);
     var cuerpo = new THREE.Mesh(cuerpoGeom, mat);
 
     //ALAS
@@ -36,23 +42,73 @@ class Mosca extends THREE.Object3D {
 
     var alaIGeometry = new THREE.ExtrudeGeometry(shape, options);
     alaIGeometry.scale(0.5,0.5,0.5);
-    alaIGeometry.translate(-0.6,0,-0.05);
+    alaIGeometry.translate(-0.9,0,-0.05);
     this.alaI = new THREE.Mesh(alaIGeometry, mat);
 
     var alaDGeometry = new THREE.ExtrudeGeometry(shape, options);
     alaDGeometry.scale(0.5,0.5,0.5);
-    alaDGeometry.translate(-0.6,0,-0.05);
+    alaDGeometry.translate(-0.9,0,-0.05);
     alaDGeometry.rotateY(Math.PI);
     this.alaD = new THREE.Mesh(alaDGeometry, mat);
 
-
-    //UNIMOS LAS PARTES DEL BRAZO.
     var moscaCSG = new CSG();
     moscaCSG.union([cuerpo, this.alaI, this.alaD]);
     var mosca = moscaCSG.toMesh();
     mosca.rotateY(Math.PI/2);
+    //FIN MOSCA
 
-    this.add(mosca);
+    var shape = new THREE.Shape();
+    shape.moveTo(0,0);
+    shape.lineTo(-0.31,0.5);
+    shape.lineTo(0.31,0.5);
+    shape.lineTo(0,0);
+
+    var options = {
+      depth: 1, // Aumentamos la profundidad para que el prisma sea más visible
+      steps: 1, // Reducimos el número de pasos para una geometría más simple
+      curveSegments: 0, // No hay segmentos curvos en una forma triangular
+      bevelSegments: 1, // Reducimos el número de segmentos del bisel para mantenerlo simple
+      bevelThickness: 0, // Grosor del bisel, puede ajustarse según el efecto deseado
+      bevelSize: 0 // Tamaño del bisel, puede ajustarse según el efecto deseado
+    };
+
+    var restaGeom = new THREE.ExtrudeGeometry(shape, options);
+    restaGeom.scale(1,1,2);
+    restaGeom.translate(0,0.5,-1);
+
+    //Para generar la forma de la corona con prismas de triangulos invertidos
+    var restas = [];
+
+    for (let i=0; i<8; i++){
+      var restaMesh = new THREE.Mesh(restaGeom, mat);
+      restaMesh.rotateY(Math.PI/5*i);
+      restas.push(restaMesh);
+    }
+
+    var baseGeometry = new THREE.CylinderGeometry(1, 1, 1, 32);
+    baseGeometry.translate(0,0.5,0);
+    var baseMesh = new THREE.Mesh(baseGeometry, mat);
+
+    var restaCSG = new CSG();
+    for (let i=0; i<restas.length; i++){
+      restaCSG.union([restas[i]]);
+    }
+    var restaTotal = restaCSG.toMesh();
+
+    // Convertir las geometrías en CSG
+    var csg = new CSG();
+    csg.union([baseMesh]);
+    csg.subtract([restaTotal]);
+    var coronaMesh = csg.toMesh();
+    coronaMesh.scale.set(0.3,0.4,0.3);
+    coronaMesh.position.y = 0.9;
+
+    var reinaCSG = new CSG();
+    reinaCSG.union([coronaMesh, mosca]);
+    var moscaReina = reinaCSG.toMesh();
+
+    // Añadir la corona a este objeto
+    this.add(moscaReina);
     
   }
   
@@ -133,4 +189,4 @@ class Mosca extends THREE.Object3D {
   }
 }
 
-export { Mosca };
+export { MoscaReina };
